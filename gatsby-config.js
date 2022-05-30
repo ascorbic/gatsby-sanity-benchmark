905db5962bdc17346b4b1235d290b0f6480736e8
Matt Kane <m@mk.gg>
2022-05-30 11:18:29 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Gatsby Sanity Benchmark`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.BENCHMARK_SANITY_PROJECT_ID,
+        dataset: process.env.BENCHMARK_SANITY_DATASET,
+      },
+    },
+  ],
+}
